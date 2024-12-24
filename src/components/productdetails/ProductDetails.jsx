69de2dc72c5a3c739c9cf0c41bf5477e36425956
abhishekkashyap18 <117872546+abhishekkashyap18@users.noreply.github.com>
2024-12-24 10:55:50 +0530

    add multiple images of product in product detail page

--- src/components/productdetails/ProductDetails.jsx
+++ src/components/productdetails/ProductDetails.jsx
@@ -12,9 +12,20 @@ function ProductDetails() {
 
   return (
     <div className="container mx-auto py-10 bg-[#e5f4e6] p-5">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-1 md:gap-10">
         <div>
           <img src={product.image} alt={product.name} className="w-full h-2/3 object-cover rounded-lg" />
+          <ul className='flex gap-5 py-4 w-full justify-center'>
+            <li className='border-solid border-2 border-black rounded-md overflow-hidden hover:cursor-pointer'>
+              <img className='hover:blur-sm' src="https://placehold.co/100x80" alt="" />
+            </li>
+            <li className='border-solid border-2 border-black rounded-md overflow-hidden hover:cursor-pointer'>
+              <img className='hover:blur-sm' src="https://placehold.co/100x80" alt="" />
+            </li>
+            <li className='border-solid border-2 border-black rounded-md overflow-hidden hover:cursor-pointer'>
+              <img className='hover:blur-sm' src="https://placehold.co/100x80" alt="" />
+            </li>
+          </ul>
         </div>
         <div className='py-1'>
           <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
